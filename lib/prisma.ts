8a10cdec1adb4fac726dcf469ae8da29c665454b
MiamiGlobalInvestmentsGroupLLC <motazbarakat2007132@gmail.com>
2025-09-06 01:57:07 +0300

    Delete lib directory

--- lib/prisma.ts
+++
@@ -1,2 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const g = global as any; export const prisma: PrismaClient = g.prisma || new PrismaClient({log:['warn','error']}); if(process.env.NODE_ENV!=='production'){ g.prisma = prisma; }
